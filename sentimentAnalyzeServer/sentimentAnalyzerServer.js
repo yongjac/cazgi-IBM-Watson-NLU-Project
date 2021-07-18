67937aa118223f27e1006e37750c2109292d45bd
yongjac <koyeja@hotmail.com>
2021-07-18 00:46:59 +0000

    Server and client side changes to implement the final project requirements

--- sentimentAnalyzeServer/sentimentAnalyzerServer.js
+++ sentimentAnalyzeServer/sentimentAnalyzerServer.js
@@ -2,6 +2,13 @@ const express = require('express');
 const dotenv = require('dotenv');
 dotenv.config();
 
+const app = new express();
+
+app.use(express.static('client'))
+
+const cors_app = require('cors');
+app.use(cors_app());
+
 function getNLUInstance() {
     let api_key = process.env.API_KEY;
     let api_url = process.env.API_URL;
@@ -19,32 +26,78 @@ function getNLUInstance() {
     return naturalLanguageUnderstanding;
 }
 
-const app = new express();
+function analyze(documentToAnalyze, analyzeParams, feature, res) {
+    let naturalLanguageUnderstanding = getNLUInstance();
 
-app.use(express.static('client'))
-
-const cors_app = require('cors');
-app.use(cors_app());
+    naturalLanguageUnderstanding.analyze(analyzeParams)
+    .then(analysisResults => {
+        let result;
+        if(feature === "sentiment") {
+            result = analysisResults.result.sentiment.document.label;
+        } else {
+            result = analysisResults.result.emotion.document.emotion;
+        }
+        res.send(result);
+    })
+    .catch(err => {
+        res.send(err.toString());
+    });
+}
 
 app.get("/",(req,res)=>{
     res.render('index.html');
-  });
+});
 
 app.get("/url/emotion", (req,res) => {
+    let url = req.query.url;
 
-    return res.send({"happy":"90","sad":"10"});
+    const analyzeParams = {
+        'url': url,
+        'features': {
+            'emotion': {}
+        }
+    };
+
+    analyze(url, analyzeParams, "emotion", res);
 });
 
 app.get("/url/sentiment", (req,res) => {
-    return res.send("url sentiment for "+req.query.url);
+    let url = req.query.url;
+
+    const analyzeParams = {
+        'url': url,
+        'features': {
+            'sentiment': {}
+        }
+    };
+
+    analyze(url, analyzeParams, "sentiment", res);
 });
 
 app.get("/text/emotion", (req,res) => {
-    return res.send({"happy":"10","sad":"90"});
+    let text = req.query.text;
+
+    const analyzeParams = {
+        'text': text,
+        'features': {
+            'emotion': {}
+        }
+    };
+
+    analyze(text, analyzeParams, "emotion", res);
 });
 
 app.get("/text/sentiment", (req,res) => {
-    return res.send("text sentiment for "+req.query.text);
+    let text = req.query.text;
+
+    const analyzeParams = {
+        'text': text,
+        'features': {
+            'sentiment': {}
+        }
+    };
+
+    analyze(text, analyzeParams, "sentiment", res);
 });
 
 let server = app.listen(8080, () => {
